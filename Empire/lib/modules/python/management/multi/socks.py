af6df09dbd7adc57836e67064626315cd2357a14
Kevin <lustic@adobe.com>
2018-05-15 16:54:06 -0600

    Added minor changes to Empire module file

--- Empire/lib/modules/python/management/multi/socks.py
+++ Empire/lib/modules/python/management/multi/socks.py
@@ -5,8 +5,7 @@ from lib.common import helpers
 
 
 class Module:
-
-    def __init__(self, mainMenu, params=[]):
+    def __init__(self, mainMenu, params=None):
 
         # metadata info about the module, not modified during runtime
         self.info = {
@@ -30,18 +29,18 @@ class Module:
             'OpsecSafe': True,
 
             # the module language
-            'Language' : 'python',
+            'Language': 'python',
 
             # Needs administrative privs
             'NeedsAdmin': False,
 
             # the minimum language version needed
-            'MinLanguageVersion' : '2.7',
+            'MinLanguageVersion': '2.7',
 
             # list of any references/other comments
             'Comments': [
                 'You must set up a standalone AlmondRocks server for this to connect to! Refer to the AlmondRocks Github project for more details.',
-                'https://github.com/klustic/AlmondRocks'
+                'Repo: https://github.com/klustic/AlmondRocks'
             ]
         }
 
@@ -51,14 +50,14 @@ class Module:
             #   value_name : {description, required, default_value}
             'Agent': {
                 # The 'Agent' option is the only one that MUST be in a module
-                'Description'   :   'Agent to run the AROX relay on',
-                'Required'      :   True,
-                'Value'         :   ''
+                'Description': 'Agent to run the AROX relay on',
+                'Required': True,
+                'Value': ''
             },
             'server': {
-                'Description'   :   'FQDN/IPv4 and port of the AROX server (e.g. 1.2.3.4:443 or hax0r.com:443)',
-                'Required'      :   True,
-                'Value'         :   ''
+                'Description': 'FQDN/IPv4 and port of the AROX server (e.g. 1.2.3.4:443 or hax0r.com:443)',
+                'Required': True,
+                'Value': ''
             },
         }
 
@@ -66,7 +65,7 @@ class Module:
         self.mainMenu = mainMenu
 
         # This is mostly in case options are passed on the command line
-        if params:
+        if params is not None:
             for param in params:
                 # parameter format is [Name, Value]
                 option, value = param
@@ -74,8 +73,11 @@ class Module:
                     self.options[option.lower()]['Value'] = value
 
     def generate(self, obfuscate=False, obfuscationCommand=""):
+        tunnel_addr = self.options['server']['Value']
+
         # Read in the module source template
-        module_source_file = os.path.join(self.mainMenu.installPath, 'data/module_source/python/management/socks-src.py')
+        module_source_file = os.path.join(self.mainMenu.installPath,
+                                          'data/module_source/python/management/socks-src.py')
         try:
             with open(module_source_file) as f:
                 module_source = f.read()
@@ -86,7 +88,7 @@ class Module:
         # Render the module_template
         module_template = string.Template(module_source)
         try:
-            module = module_template.substitute(TUNNEL_ADDR=self.options['server']['Value'])
+            module = module_template.substitute(TUNNEL_ADDR=tunnel_addr)
         except KeyError as e:
             print helpers.color("[!] Error rendering module template: {0}".format(e))
             return ''
